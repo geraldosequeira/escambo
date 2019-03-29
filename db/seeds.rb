af3cb4b3448bd4b2433c74e1829b9df93329ee92
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-03-29 15:55:50 -0300

    add model with migrate and seeds for Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Cadastrando Categorias..."
+categories = [
+  "Animais e acessórios",
+  "Esportes",
+  "Para a sua casa",
+  "Eletrônicos e celulares",
+  "Música e hobbies",
+  "Bebês e crianças",
+  "Moda e beleza",
+  "Veículos e barcos",
+  "Imóveis",
+  "Empregos e negócios"
+]
+
+categories.each do |description|
+  Category.find_or_create_by( description: description )
+end
+p "...[OK!]"
