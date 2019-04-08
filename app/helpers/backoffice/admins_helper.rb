89f20b1cc00a6974bbf86adf025728616a44a22e
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-04-08 20:14:57 -0300

    Add policy radio collect and modify collect for map

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -3,12 +3,15 @@ module Backoffice::AdminsHelper
   OptionsForRoles = Struct.new(:id, :description)
 
   def options_for_roles
-    options = []
+    # options = []
+    # Admin.roles_i18n.each do |key, value|
+    #   options.push(OptionsForRoles.new(key, value))
+    # end
+    # options
 
-    Admin.roles_i18n.each do |key, value|
-      options.push(OptionsForRoles.new(key, value))
+    Admin.roles_i18n.map do |key, value|
+      OptionsForRoles.new(key, value)
     end
 
-    options
   end
 end
