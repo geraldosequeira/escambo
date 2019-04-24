c9cc417275bd972a5eaf234218a19eee75d725a5
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-04-24 11:26:55 -0300

    Added description Markdown and short description

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,10 +1,13 @@
 class Ad < ApplicationRecord
+
+  before_save :md_to_hml
+
   belongs_to :category
   belongs_to :member
 
   validates_presence_of  :picture
 
-  validates :title, :description, :category, presence: true
+  validates :title, :description_md, :description_short, :category, presence: true
   validates :picture, :finish_date, presence: true
   validates :price, numericality: { greater_than: 0 }
 
@@ -18,4 +21,24 @@ class Ad < ApplicationRecord
                      styles: { medium: "320x150#", thumb: "100x100>", large: "900X400>" },
                      default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+  private
+    def md_to_hml
+      options = {
+          filter_html: true,
+          link_attributes: {
+            rel: "nofollow",
+            target: "_blank"
+          }
+      }
+
+      extensions = {
+        space_after_headers: true,
+        autolink: true
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+      self.description = markdown.render(self.description_md)
+    end
 end
--- lib/tasks/utils.rake
+++ lib/tasks/utils.rake
@@ -59,7 +59,8 @@ namespace :dev do
     5.times do
       Ad.create!(
           title: Faker::Lorem.sentence(1),
-          description: markdown_fake,
+          description_short: Faker::Lorem.sentence([2,3].sample),
+          description_md: markdown_fake,
           member: Member.first,
           category: Category.all.sample,
           price: "#{Random.rand(500)},#{99}",
@@ -72,7 +73,8 @@ namespace :dev do
       10.times do
         Ad.create!(
           title: Faker::Lorem.sentence(1),
-          description: markdown_fake,
+          description_short: Faker::Lorem.sentence([2,3].sample),
+          description_md: markdown_fake,
           member: Member.all.sample,
           category: Category.all.sample,
           price: "#{Random.rand(500)},#{99}",
