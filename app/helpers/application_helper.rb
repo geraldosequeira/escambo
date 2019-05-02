9bd8c9c4a0bc7e92e5db42fe7cdb2fdb5e1d0038
Geraldo Sequeira <gcm.sequeira@gmail.com>
2019-05-02 17:03:17 -0300

    unifying login and registration screens

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def devise_sign_in?
+    params[:controller] == "members/sessions" ? "" : "display:none;"
+  end
+
+  def devise_sign_up?
+    params[:controller] == "members/registrations" ? "" : "display:none;"
+  end
 end
